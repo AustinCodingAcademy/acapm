6189d72171c5860c146567d341c37670382b68d8
Wade Cosby <wadecosby@wades-mbp.attlocal.net>
2018-04-26 22:49:09 -0500

    adds questions and array to hold response

--- index.js
+++ index.js
@@ -5,10 +5,29 @@
 // Create a string that is a json object
 // The key values should be the inputs you get from the user
 // Save this json to a file called package.json
+const readline = require('readline');
 
-console.log(process.argv)
-process.argv.forEach((process,i,arr)=>{
-  console.log(`${i}: ${process}`)
-})
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-process.argv[2]==='init' ? console.log("hi there") : console.log("nope")
+const response = []
+
+process.argv[2]==='init' 
+    ? (rl.question(`What is your yearly income? `, (answer1) => {
+        rl.question(`How much do you spend on groceries? `, (answer2) => {
+          rl.question(`How much do you spend on gas? `, (answer3) => {
+            rl.question(`How much do you spend on rent? `, (answer4) => {
+              rl.question(`How much do you spend on entertainment? `, (answer5) => {
+                response.push(answer1, answer2, answer3, answer4, answer5)
+                console.log(response)
+                rl.close()
+              })
+            })
+          })
+        })
+      })
+    )
+    : console.log(`Did you mean to input init? \n\n try: \t node index.js init \n`)
+
