df7bb127e6f1b4f22fccabfd6e058a7074150872
Wade Cosby <wadecosby@wades-mbp.attlocal.net>
2018-04-26 23:27:22 -0500

    adds json object with ability to save file

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Create a node program x
 // Check for the first process argument to be init x
-// If the first argument is init continue processing <---------
-// Use readline to ask for 5 inputs
-// Create a string that is a json object
+// If the first argument is init continue processing x
+// Use readline to ask for 5 inputs x
+// Create a string that is a json object 
 // The key values should be the inputs you get from the user
 // Save this json to a file called package.json
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-const response = []
 
 process.argv[2]==='init' 
     ? (rl.question(`What is your yearly income? `, (answer1) => {
@@ -20,8 +20,18 @@ process.argv[2]==='init'
           rl.question(`How much do you spend on gas? `, (answer3) => {
             rl.question(`How much do you spend on rent? `, (answer4) => {
               rl.question(`How much do you spend on entertainment? `, (answer5) => {
-                response.push(answer1, answer2, answer3, answer4, answer5)
-                console.log(response)
+                const response = {
+                  'salary' : answer1, 
+                  'groceries' : answer2, 
+                  'gas' : answer3,
+                  'rent' : answer4,
+                  'entertainment' : answer5
+                }
+                const obj = JSON.stringify(response)
+                fs.writeFile('message.json', obj, (err) => {
+                  if (err) throw err;
+                  console.log('The file has been saved!');
+                });
                 rl.close()
               })
             })
