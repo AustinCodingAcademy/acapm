ad3fcb7a9313a707a49bf292b64b1e2a2c489967
Ty Sweet <tysweet77@gmail.com>
2018-04-28 11:17:08 -0500

    working

---
+++ index.js
@@ -0,0 +1,35 @@
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+process.argv[2] === 'init'
+    ? (rl.question('Name?', (answer1) => {
+        rl.question('Version?', (answer2) => {
+            rl.question('Description?', (answer3) => {
+                rl.question('Author?', (answer4) => {
+                    rl.question('Keywords? ', (answer5) => {
+                        const response = {
+                            'name':  answer1,
+                            'version': answer2,
+                            'description': answer3,
+                            'author': answer4,
+                            'keywords': answer5
+                        }
+                        const obj = JSON.stringify(response, '', '\t')
+                        fs.writeFile('package.json', obj, (err) => {
+                            if (err) throw err;
+                            console.log('A new package.json file has been created.  Have a nice day!')
+                        });
+                        rl.close();
+                    })
+                })
+            })
+        })
+    })
+)
+  :console.log(`Did you mean to input init? \n\n try: \t node index.js init`);
+ 
