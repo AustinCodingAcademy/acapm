595e0bb9bcd9895c962e7ae066ce72da00d2fe21
walzer85 <walzer85@gmail.com>
2018-04-28 09:20:34 -0500

    adding the user inputs after init

---
+++ index.js
@@ -0,0 +1,21 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+   });   
+
+console.log(process.argv);
+process.argv.forEach(function (val, index, array) {
+ console.log(index + ': ' + val);
+});
+
+if(process.argv[2] === "init"){
+  //do init stuff
+  rl.question('Tell me five random facts about youself, please!', (fact1, fact2, fact3, fact4, fact5) => {
+    console.log(`Thank you.  You will be spared in the uprising.`);
+    console.log(`Here is the file we have on you:`)
+    rl.close();
+   });
+   
+}
