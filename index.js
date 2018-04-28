0b56563690e2de12a9467e0af385949b5d7378fc
walzer85 <walzer85@gmail.com>
2018-04-28 14:15:41 -0500

    creating the json correctly, but not populating it correctly.  Researching a solution

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ const rl = readline.createInterface({
 });
 
 if(process.argv[2] === "init"){
-  //do init stuff
-  rl.question('Tell me five random facts about youself, please! List them with commas', (fact1, fact2, fact3, fact4, fact5) => {
-    console.log(`Thank you.  You will be spared in the uprising.`);
-    console.log(`Here is the file we have on you:` )
-    rl.close();
+    //do init stuff
+    rl.question('Tell me five random facts about youself, please! Separate them with commas', (fact1, fact2, fact3, fact4, fact5) => {
+        console.log(`Thank you.  You will be spared in the uprising.`);
+        rl.close();
+        var fs = require('fs');
+    fs.writeFile("yourfacts.json", rl.question(fact1, fact2, fact3, fact4, fact5), (err) => {
+        if (err) throw err;
+        console.log("Here is the file we have on you.");
+        });
    });   
 }
-
-var fs = require('fs');
-fs.writeFile("mynewfile.txt", "Important Info", (err) => {
-   if (err) throw err;
-   console.log("The file was succesfully saved!");
-});
