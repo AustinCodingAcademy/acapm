503fc7196bc3c2d6e5c65dc42ebeaec6282edd20
Wade Cosby <wadecosby@Wades-MacBook-Pro.local>
2018-04-28 11:33:26 -0500

    adds default responses

--- index.js
+++ index.js
@@ -7,38 +7,38 @@
 // Save this json to a file called package.json x
 const readline = require('readline');
 const fs = require('fs');
+const path = require('path')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-console.log(process.argv)
-
 process.argv[2]==='init' 
-    ? (rl.question(`App name? `, (name) => {
-        rl.question(`Description? `, (desc) => {
-          rl.question(`Release version? `, (version) => {
-            rl.question(`Author? `, (author) => {
-              rl.question(`Main file path? `, (main) => {
-                const response = {
-                  'name' : name,  
-                  'description' : desc, 
-                  'version' : version,
-                  'author' : author,
-                  'main' : main
-                }
-                const obj = JSON.stringify(response, '', '\t')
-                fs.writeFile('package.json', obj, (err) => {
-                  if (err) throw err;
-                  console.log('A new package.json file has been saved');
-                });
-                rl.close()
+    ? (rl.write('myApp'),
+        rl.question(`App name? `, (name) => {
+          rl.question(`Description? `, (desc) => { rl.write('1.0.0')
+            rl.question(`Release version? `, (version) => {
+              rl.question(`Author? `, (author) => { rl.write(path.basename(__filename))
+                rl.question(`Main file path? `, (main) => {
+                  const response = {
+                    'name' : name,  
+                    'description' : desc, 
+                    'version' : version,
+                    'author' : author,
+                    'main' : main
+                  }
+                  const obj = JSON.stringify(response, '', '\t')
+                  fs.writeFile('package.json', obj, (err) => {
+                    if (err) throw err;
+                    console.log('A new package.json file has been saved');
+                  });
+                  rl.close()
+                })
               })
             })
           })
         })
-      })
-    )
+      )
     : console.log(`Did you mean to input init? \n\n try: \t node index.js init \n`)
 
