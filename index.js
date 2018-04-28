c3dd1ec2252bf7a1148d1f6bd5ec2a32121bb9fd
walzer85 <walzer85@gmail.com>
2018-04-28 13:49:35 -0500

    imported the fs code

--- index.js
+++ index.js
@@ -11,10 +11,15 @@ const rl = readline.createInterface({
 
 if(process.argv[2] === "init"){
   //do init stuff
-  rl.question('Tell me five random facts about youself, please!', (fact1, fact2, fact3, fact4, fact5) => {
+  rl.question('Tell me five random facts about youself, please! List them with commas', (fact1, fact2, fact3, fact4, fact5) => {
     console.log(`Thank you.  You will be spared in the uprising.`);
     console.log(`Here is the file we have on you:` )
     rl.close();
-   });
-   
+   });   
 }
+
+var fs = require('fs');
+fs.writeFile("mynewfile.txt", "Important Info", (err) => {
+   if (err) throw err;
+   console.log("The file was succesfully saved!");
+});
