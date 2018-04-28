134910c35ccf356bb7e702f66c164ce90083730a
walzer85 <walzer85@gmail.com>
2018-04-28 09:25:24 -0500

    gotta take a break to run some more errands, yay weekend...

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const rl = readline.createInterface({
     output: process.stdout
    });   
 
-console.log(process.argv);
-process.argv.forEach(function (val, index, array) {
+   process.argv.forEach(function (val, index, array) {
  console.log(index + ': ' + val);
 });
 
@@ -14,7 +13,7 @@ if(process.argv[2] === "init"){
   //do init stuff
   rl.question('Tell me five random facts about youself, please!', (fact1, fact2, fact3, fact4, fact5) => {
     console.log(`Thank you.  You will be spared in the uprising.`);
-    console.log(`Here is the file we have on you:`)
+    console.log(`Here is the file we have on you:` )
     rl.close();
    });
    
