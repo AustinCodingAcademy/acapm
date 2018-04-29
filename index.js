309b5ffc9993dc4b23762d0c4510adf3fd326c62
walzer85 <walzer85@gmail.com>
2018-04-29 13:18:09 -0500

    spoke with Jordan for clarification on json.stringify passing arguments correctly, now sends a string to the new txt file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if(process.argv[2] === "init"){
         console.log(`Thank you.  You will be spared in the uprising.`);
         rl.close();
         var fs = require('fs');
-    fs.writeFile("package.json", JSON.stringify(rl.question(fact1, fact2, fact3, fact4, fact5)), (err) => {
+    fs.writeFile("package.txt", JSON.stringify(fact1, fact2, fact3, fact4, fact5), (err) => {
         if (err) throw err;
         console.log("Here is the file we have on you.");
         });
