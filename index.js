061062678be8cb08331485e75ec2ee2293644d68
mimitch2 <mike@melodix.com>
2018-04-30 12:26:35 -0500

    acapm homework commit

---
+++ index.js
@@ -0,0 +1,47 @@
+
+const readline = require('readline');
+const fs = require('fs');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const myJsonData = {
+  "name" : "", 
+  "from" : "", 
+  "color" : "", 
+  "team" : "", 
+  "beer" : ""
+}
+
+const writeMyJson=(data)=>{
+  fs.writeFile("package.json", data, (err) => {
+    if (err) throw err;
+    console.log("The file was succesfully saved!");
+  });
+}
+
+if(process.argv[2] === "init"){
+  rl.question("What is your name? ", (answer) => {
+    myJsonData['name'] = answer;
+    rl.question("Where are you from? ", (answer) => {
+      myJsonData['from'] = answer
+      rl.question("Where are your favorite color? ", (answer) => {
+        myJsonData['color'] = answer
+        rl.question("What is your favorite team? ", (answer) => {
+          myJsonData['team'] = answer
+          rl.question("What is your fovorite beer? ", (answer) => {
+            myJsonData['beer'] = answer
+            rl.close();
+            writeMyJson(JSON.stringify(myJsonData))
+          });
+        });
+      });
+    });
+  });
+}else{
+  console.log("Please type 'node index.js init'");
+  process.exit();
+}
