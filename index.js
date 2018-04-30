36d24d7f60712c8b8d1916fa93f5a70678cd1580
Nathan Simmons <nathanhbsimmons@gmail.com>
2018-04-30 11:20:55 -0500

    pacakge manager program complete

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+let jsonObject = {
+    'name': '',
+    'age': '',
+    'favoriteBand': '',
+    'favoriteAuthor': '',
+    'meaningOfLife': '',
+}
+
+const createJson =(jsonObject)=> {
+   
+    var fs = require('fs');
+    fs.writeFile("package.json", JSON.stringify(jsonObject), (err) => {
+       if (err) throw err;
+   
+    });
+}
+
+
+const createPrompts =()=> {
+
+    const readline = require('readline');
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+
+    rl.question('What is your name? ', (answer) => {
+        jsonObject.name = answer
+        rl.question('What is your age? ', (answer) => {
+            jsonObject.age = answer
+            rl.question('What is your favorite band? ', (answer) => {
+                jsonObject.favoriteBand = answer
+                rl.question('What is your favorite author? ', (answer) => {
+                    jsonObject.favoriteAuthor = answer
+                    rl.question('What is the meaning of life? ', (answer) => {
+                        jsonObject.meaningOfLife = answer
+                            createJson(jsonObject);
+                            rl.close();
+                        });
+                    });
+                });
+            });
+        });
+}
+                            
+
+
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+   });
+   
+   if(process.argv[2] === "init"){
+    
+    createPrompts();
+   }
+
+   
