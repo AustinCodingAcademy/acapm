f45a18ab8af025be10c31a31a2186ecd7e1efff7
walzer85 <walzer85@gmail.com>
2018-05-15 14:02:21 -0500

    working finally

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const readline = require('readline');
 const fs = require("fs");
+const path = require("path");
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
    });   
 
-   process.argv.forEach(function (val, index, array) {
- console.log(index + ': ' + val);
-});
+//    process.argv.forEach(function (val, index, array) {
+//  console.log(index + ': ' + val);
+// });
 
 const userFacts = {
     "fact one" : "",
@@ -20,30 +21,29 @@ const userFacts = {
 
 const userPrompts = ["Fact 1: ", "Fact 2: ", "Fact 3: ", "Fact 4: ", "Fact 5: "];
 
-const contentOfJson = (facts) => {
-    fs.writeFile("package.json", data, (err) => {
+const robotFile = (facts) => {
+    fs.writeFile("package.json", facts, (err) => {
         if(err) throw (err);
-        console.log ("Thank you for your submission.  You will be spared in the uprising.")
     });
+    console.log ("Thank you for your submission.  You will be spared in the uprising.")
 };
 
-const askUser = (i) {
-//Check to see if the user has input all entries
-if(i<=Object.keys(userFacts).length) {
-    if(process.argv[2] === "init"){
-
-    }   
+const askUser = (i) => {
+    //check to see if the user has input all of their facts
+    if (i <= Object.keys(userFacts).length){
+        //initialize
+        if (process.argv[2] === 'init'){
+            //ask the user for each fact
+            rl.question(userPrompts[i], (newFact)=>{
+                userFacts[Object.keys(userFacts)[i]] = newFact;
+                return askUser(i+1)
+            })
+        } if (i == Object.keys(userFacts).length) {
+            rl.close();
+            robotFile(JSON.stringify(userFacts, null, 4))
+        }
+    } else {
+        process.exit();
+    }
 }
-}
-//initialize
-
-//still need to get the inputs in an object, currently just in a string
-//function getPrompt()
-//make a recursive function - a function that calls itself
-
-// const getPrompt = () => {
-//     rl.question('tell me something about yourself', (anser) => {
-//         myJsonData['fact'] = answer;
-//         getPrompt();
-//     })
-// }
+askUser(0)
