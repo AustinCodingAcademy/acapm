76995e8cac51b556908f0e02802fd1b9c31be1f5
Alan Houston <alanhouston5000@gmail.com>
2019-01-18 17:23:38 -0600

    working, but no file check, code not pretty

--- index.js
+++ index.js
@@ -7,29 +7,89 @@ const rl = readline.createInterface({
     output: process.stdout  
 });
 
-rl.question('',(answer)=>{
+const jsonPackage = {
+    fileInfo: []
+}
+
+rl.question('Type "init" ',(answer)=>{
     let splitAnswer = answer.split(' ');
-    if(checkForInit(splitAnswer)){
-        runInit();
+    if(checkForInit(splitAnswer) && fileDoesNOTExist()){
+        saveInputs();
     }else rl.close();
 });
 
-// var fs = require('fs');
-// fs.writeFile("mynewfile.txt", "Important Info", (err) => {
-//    if (err) throw err;
-//    console.log("The file was succesfully saved!");
-// });
-
-
-const checkForInit=(firstWord)=>{
+function checkForInit(firstWord){
     return firstWord[0] === 'init';
 };
 
-const runInit = ()=>{
-    for(let i=0;i<6;i++){
-        rl.question('Give input',(answer[i+1])=>{
-            //${answer}
-            //node step by step slide 29
-        });
-    }    
+function fileDoesNOTExist(){
+    return true; //for now
 };
+
+// function saveInputs(){
+//     rl.question('Give input: ',(answer)=>{
+//         jsonPackage.fileInfo.push(answer);
+//     });
+// }
+
+function saveInputs(){
+    rl.question('Give input: ',(a1)=>{
+        rl.question('Give input: ',(a2)=>{
+            rl.question('Give input: ',(a3)=>{
+                rl.question('Give input: ',(a4)=>{
+                    rl.question('Give input: ',(a5)=>{
+                        jsonPackage.fileInfo.push(a1);
+                        jsonPackage.fileInfo.push(a2);
+                        jsonPackage.fileInfo.push(a3);
+                        jsonPackage.fileInfo.push(a4);
+                        jsonPackage.fileInfo.push(a5);
+                        createJSONfile();
+                    });
+                });
+            });       
+        });
+    });
+};
+
+function createJSONfile(){
+    const stringedInfo = JSON.stringify(jsonPackage.fileInfo)
+    var fs = require('fs');
+    fs.appendFile("package.json", stringedInfo, (err) => {
+        if (err) throw err;
+    });
+}
+
+
+
+
+
+
+
+
+// function runInitialInit(){
+//     rl.question('Give input',(answer)=>{
+//         sendToCreateFile(answer);
+//     });     
+// };
+
+// function runOtherInits(){
+//     rl.question('Give input',(answer)=>{
+//         sendToAppendFile(answer);
+//     }); 
+// };
+
+// function sendToCreateFile(answer){
+//     var fs = require('fs');
+//     fs.writeFile("package.json", answer, (err) => {
+//         if (err) throw err;
+//     });
+// };
+
+// function sendToAppendFile(answer){
+//     let createFile = [];
+//     createFile.push(answer);
+//     var fs = require('fs');
+//     fs.appendFile("package.json", createFile, (err) => {
+//         if (err) throw err;
+//     });
+// }
