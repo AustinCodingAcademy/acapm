5af27301276add1ff41f2b2ecabe3fe3f3d86aca
unknown <erlanggaramzy@gmail.com>
2019-01-18 23:51:23 -0600

    Erlangga Ramzy

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 var readline = require('readline');
 var fs = require('fs');
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const jsonObj = {};
+
 if(process.argv[2] === "init") {
     console.log('init');
 } else {
@@ -9,31 +16,36 @@ if(process.argv[2] === "init") {
 
 fs.stat('./package.json', function(err) {  
     if (err) {
-        console.log('file does not exist')
+        // console.log('file does not exist')
     } else {
         process.exit();
     }
 });
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.question('package name: ', (answer1) => {
-    rl.question('version: ', (answer2) => {
-        rl.question('description: ', (answer3) => {
-            rl.question('entry point: ', (answer4) => {
-                rl.question('author: ', (answer5) => {
-                    
+rl.question('package name: ', (answer) => {
+    jsonObj['packageName'] = answer;
+    rl.question('version: ', (answer) => {
+        jsonObj['version'] = answer;
+        rl.question('description: ', (answer) => {
+            jsonObj['description'] = answer;
+            rl.question('entry point: ', (answer) => {
+                jsonObj['entryPoint'] = answer;
+                rl.question('author: ', (answer) => {
+                    jsonObj['author'] = answer;
+                    console.log(jsonObj);
+                    saveFile();
+                    rl.close();
                 })
             })
         })
     })
-    // rl.close();
 });
 
-// fs.writeFile('package.json', data, (err) => {
-//     if (err) throw err;
-//     console.log('The file has been saved!');
-//   });
+function saveFile() {
+    const string = JSON.stringify(jsonObj);
+    fs.writeFile('package.json', string, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
