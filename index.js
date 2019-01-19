bc8b4cdbc10facb3cbda6773b09b0c7d0467710b
unknown <erlanggaramzy@gmail.com>
2019-01-18 23:31:39 -0600

    Erlangga Ramzy

--- index.js
+++ index.js
@@ -5,17 +5,35 @@ if(process.argv[2] === "init") {
     console.log('init');
 } else {
     process.exit();
-}
+};
 
+fs.stat('./package.json', function(err) {  
+    if (err) {
+        console.log('file does not exist')
+    } else {
+        process.exit();
+    }
+});
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// rl.question('What do you think of Node.js? ', (answer) => {
-//   // TODO: Log the answer in a database
-//   console.log(`Thank you for your valuable feedback: ${answer}`);
+rl.question('package name: ', (answer1) => {
+    rl.question('version: ', (answer2) => {
+        rl.question('description: ', (answer3) => {
+            rl.question('entry point: ', (answer4) => {
+                rl.question('author: ', (answer5) => {
+                    
+                })
+            })
+        })
+    })
+    // rl.close();
+});
 
-//   rl.close();
-// });
+// fs.writeFile('package.json', data, (err) => {
+//     if (err) throw err;
+//     console.log('The file has been saved!');
+//   });
