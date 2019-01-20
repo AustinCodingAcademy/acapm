5a8b0741f040af86e61561bd3b2108f92a820ccd
katiemacaulay <katiemmacaulay@gmail.com>
2019-01-20 11:39:56 -0600

    trying out mapseries

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const readline = require('readline');
-var waterfall = require("async/waterfall");
+const async = require("async");
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -9,43 +9,42 @@ const rl = readline.createInterface({
 
 const fs = require('fs');
 
-if(process.argv[2] === "init"){
-    rl.question('What is your favorite food? ', (food) => {
-        rl.question('What is your favorite color? ', (color) => {
-            rl.question('What is your favorite car? ', (car) => {
-                rl.question('What is your favorite animal? ', (animal) => {
-                    rl.question('What is your favorite video game? ', (videoGame) => {
-                        const answers = {food, color, car, animal, videoGame};
-                        const obj = JSON.stringify(answers)
-                        fs.writeFile('package.json', obj, (err) => {
-                            if (err) throw err;
-                                console.log('a new package.json file has been saved')
-                        });
-                        rl.close();
-                    })  
-                })
-            })
-        })
-    })
-}
-
-
-// waterfall([
-//     function(callback) {
-//         rl.question('What is your favorite food1? ', (answer) => {
-//             callback(null, answer)
+// if(process.argv[2] === "init"){
+//     rl.question('What is your favorite food? ', (food) => {
+//         rl.question('What is your favorite color? ', (color) => {
+//             rl.question('What is your favorite car? ', (car) => {
+//                 rl.question('What is your favorite animal? ', (animal) => {
+//                     rl.question('What is your favorite video game? ', (videoGame) => {
+//                         const answers = {food, color, car, animal, videoGame};
+//                         const obj = JSON.stringify(answers)
+//                         fs.writeFile('package.json', obj, (err) => {
+//                             if (err) throw err;
+//                                 console.log('a new package.json file has been saved')
+//                         });
+//                         rl.close();
+//                     })  
+//                 })
+//             })
 //         })
-//     },
-//     function(answer, callback) {
-//         rl.question('What is your favorite food2? ', (answer) => {
-//             callback(null, answer)
-//         })
-//     },
-//     function(arg1, callback) {
-//         rl.question('What is your favorite food3? ', (answer) => {
-//             callback(null, answer)
-//         })
-//     }
-// ], function (err, result) {
-//     console.log(result)
-// });
+//     })
+// }
+const questions = [
+    'What is your favorite food? ',
+    'What is your favorite color? ',
+    'What is your favorite car? ',
+    'What is your favorite animal? ',
+    'What is your favorite video game? ',
+]
+// let answers = []
+// async.mapSeries(questions, (q, callback) => {
+//     console.log('question: ', q)
+//     rl.question(q, (answer) => {
+//         console.log('answer: ', answer)
+//         answers.push(answer)
+//         callback(null, answer)
+//     })
+// }, () => {
+//     rl.close();
+//     console.log(answers)
+//     console.log('done')
+// })
