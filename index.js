3ed1a84dc10fbb6f77ebb4bd60efc85c266e4938
Laura Colten <lauracolten5@gmail.com>
2019-01-20 23:13:26 -0600

    added json object

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+const numbers = {};
+
 rl.question('Please enter "npm init": ', (npmInit) => {
     if (npmInit !== 'npm init') {
         process.exit();    
@@ -14,10 +16,26 @@ rl.question('Please enter "npm init": ', (npmInit) => {
         process.exit();    
     }
     
-    rl.question('Please enter 5 numbers: ', (nums) => {
-        console.log(`${nums}`);
+    rl.question('Please enter 5 numbers. Number 1: ', (num1) => {
+        numbers.number1 = num1;
+        rl.question('Number 2: ', (num2) => {
+            numbers.number2 = num2;
+            rl.question('Number 3: ', (num3) => {
+                numbers.number3 = num3;
+                rl.question('Number 4: ', (num4) => {
+                    numbers.number4 = num4;
+                    rl.question('Number 5: ', (num5) => {
+                        numbers.number5 = num5;
+                        fs.writeFileSync("package.json", JSON.stringify(numbers, null, 2));
+                        return rl.close();
+                    })
+                })
+            })
+
+        })
     })
 })
+
 
 
 
