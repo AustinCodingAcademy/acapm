bda3afcc1a0648b85ba5ec09f5a2a565c4e6b796
Darwin Risser <riser.darwin@gmail.com>
2019-01-21 11:15:45 -0600

    basic functionality added

--- index.js
+++ index.js
@@ -8,10 +8,56 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-if(process.argv[0] === "init"){
-    //do init stuff
-   console.log("I am doing init stuff");
-  }
+if(process.argv[2] === "init"){
+ console.log("Get ready to init!");
+ rl.question('What is your name? ', (name) => {
+  rl.question('What is your e-mail address? ', (email) => {
+    rl.question('What is your username? ', (username) => {
+      rl.question('What is your password? ', (password) => {
+        rl.question('What is your favorite color? ', (color) => {
+          let answers = {  
+            name: name,
+            email: email, 
+            username: username,
+            password: password,
+            color: color
+           };
+           console.log("Created new package.JSON file:");
+           console.log(answers);
+        let data = JSON.stringify(answers);  
+        fs.writeFileSync('package.json', data);  
+          rl.close();
+        }) 
+      })
+    })
+  })
+ })
+} else {
+  console.log("error")
+  rl.close();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if(process.argv[0] === "init"){
+//     //do init stuff
+//    console.log("I am doing init stuff");
+//   }
 
 // rl.question('What do you want to do? ', (answer) => {
 //   if (answer === 'init'){
