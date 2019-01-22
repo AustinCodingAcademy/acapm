3c8f0420afe9902378a49de91d72a35a456accaa
Abby Gottlich <abby.gottlich@gmail.com>
2019-01-22 15:16:10 -0600

    added JSON object, converted to string, created new package

--- README.md
+++ README.md
@@ -3,12 +3,12 @@
 
 * Create a node program
 * Check for the first process argument to be init
-*** If the first argument is init continue processing else end the program
-*** check if a file named package.json already exists, if it does exit the program
+* If the first argument is init continue processing else end the program
+* check if a file named package.json already exists, if it does exit the program
 * Use readline to ask for 5 inputs
-*** Create a string that is a json object
-*** The key values should be the inputs you get from the user
-*** Save this json to a file called package.json
+* Create a string that is a json object
+* The key values should be the inputs you get from the user
+* Save this json to a file called package.json
 
 # Bonus
 * Don't hardcode 5 readlines
--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-// console.log(process.argv);
-// process.argv.forEach(function(val, index, array) {
-//   console.log(index + ": " + val);
-// });
-
 // checking for first arg
 if (process.argv[2] === "init") {
-  // check to see if package.json already exists, else exit program
   // prompt questions
   const readline = require("readline");
+  const fs = require("fs");
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
   function askQuestion() {
-    rl.question("Project Name: ", answer => {
-      rl.question("Author Name: ", answer => {
-        rl.question("Version: ", answer => {
-          rl.question("ID: ", answer => {
-            rl.question("Favorite Color: ", answer => {
-              console.log("Prompts completed.");
+    rl.question("Project Name: ", name => {
+      rl.question("Author Name: ", author => {
+        rl.question("Version: ", version => {
+          rl.question("ID: ", id => {
+            rl.question("Favorite Color: ", color => {
+              // saved responses in JSON object
+              const response = {
+                "Project Name": name,
+                "Author Name": author,
+                "Version": version,
+                "ID": id,
+                "Favorite Color": color
+              };
+              // JSON object converted to string and new package created
+              const obj = JSON.stringify(response, "", "\t")
+              fs.writeFile("package.json", obj, (err) => {
+                if (err) throw err;
+                console.log("The file was successfully saved!");
+              });
+              rl.close();
             });
           });
         });
@@ -29,24 +38,3 @@ if (process.argv[2] === "init") {
   }
   askQuestion();
 }
-// if first arg is not "init", exit the program
-
-// writing file
-// let fs = require("fs");
-
-// fs.writeFile("myNewFile.txt", "Important Info", err => {
-//   if (err) throw err;
-//   console.log("The file was successfully saved!");
-// });
-
-// reading file
-// const fs = require("fs");
-
-// fs.readFile("myNewFile.txt", "utf8", fileWasRead);
-
-// function fileWasRead(err, data) {
-//   if (err) {
-//     return console.log(err);
-//   }
-//   console.log(data);
-// }
